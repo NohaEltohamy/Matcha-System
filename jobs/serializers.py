b815b8bdcda90df1a4a4b6ddc59444a077970901
josspheenboles <eng.jospheenboles@gmail.com>
2025-10-24 12:27:25 +0300

    candidate cv & matched jobs srlizer

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -2,6 +2,7 @@
 
 from rest_framework import serializers
 from .models import Job
+from .models import Job, CV, CandidateJobMatch
 
 class JobSerializer(serializers.ModelSerializer):
     recruiter_username = serializers.CharField(source='recruiter.username', read_only=True)
@@ -25,6 +26,61 @@ class JobSerializer(serializers.ModelSerializer):
             "recruiter",
             "opened_at",
             "closed_at",
+            "created_at",
+            "updated_at",
+        ]
+
+
+
+
+
+class CVSerializer(serializers.ModelSerializer):
+    candidate_name = serializers.CharField(source='candidate.name', read_only=True)
+    candidate_email = serializers.CharField(source='candidate.email', read_only=True)
+    
+    class Meta:
+        model = CV
+        fields = [
+            "id",
+            "candidate",
+            "candidate_name", 
+            "candidate_email",
+            "title",
+            "file_url",
+            "file_name",
+            "file_size",
+            "uploaded_at",
+            "updated_at",
+        ]
+        read_only_fields = [
+            "candidate",
+            "uploaded_at",
+            "updated_at",
+        ]
+
+class CandidateJobMatchSerializer(serializers.ModelSerializer):
+    cv_data = CVSerializer(source='cv', read_only=True)
+    job_title = serializers.CharField(source='job.title', read_only=True)
+    job_id = serializers.IntegerField(source='job.id', read_only=True)
+    
+    class Meta:
+        model = CandidateJobMatch
+        fields = [
+            "id",
+            "cv",
+            "cv_data",
+            "job",
+            "job_id",
+            "job_title",
+            "genai_score",
+            "skills_match_score",
+            "experience_match_score",
+            "overall_fit_score",
+            "ai_analysis",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = [
             "created_at",
             "updated_at",
         ]
